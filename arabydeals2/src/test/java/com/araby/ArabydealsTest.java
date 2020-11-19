de6515a0f428b5edba6e97f6fb84d2b959b2e445
mohamed <m.ibrahem@tap.company>
2020-11-19 15:55:32 +0200

    b

--- arabydeals2/src/test/java/com/araby/ArabydealsTest.java
+++ arabydeals2/src/test/java/com/araby/ArabydealsTest.java
@@ -1,11 +1,8 @@
 package com.araby;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 import com.araby.utils.Constant;
 
@@ -24,9 +21,9 @@ public class ArabydealsTest {
 		driver.manage().window().maximize();
 	}
 
-	private boolean checkURL(String keyword) {
-		return driver.getCurrentUrl().contains(keyword);
-	}
+//	private boolean checkURL(String keyword) {
+//		return driver.getCurrentUrl().contains(keyword);
+//	}
 
 //	@Test(priority = 1)
 //	public void checkAboutArabyDealsURL() throws InterruptedException {
